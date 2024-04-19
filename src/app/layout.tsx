f3c40036e63280e5cc3ebdbad49270a451c17ba1
luhoro <luisashorowitz@gmail.com>
2024-04-18 21:19:59 -0300

    Build: Clean base project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.scss"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "CS Motors - Sua oficina especializada",
+  description: "Oficina de carros em Minas Gerais",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
