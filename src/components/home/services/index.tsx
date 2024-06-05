81a4414601a36aed3cd1c70ff7c706ce7c80ac28
luhoro <luisashorowitz@gmail.com>
2024-06-05 20:53:24 -0300

    Feat: Services section

--- src/components/home/services/index.tsx
+++ src/components/home/services/index.tsx
@@ -7,24 +7,49 @@ import Link from "next/link"
 
 const Services = ({ object }: HomeProps) => {
   return (
-    <Container>
-      <section className={styles.containerAbout}>
-        <article className={styles.innerAbout}>
-          <h2>Sobre</h2>
-          <p>{object.metadata.about.description}</p>
-        </article>
+    <>
+      <Container>
+        <section className={styles.containerAbout}>
+          <article className={styles.innerAbout}>
+            <h2>Sobre</h2>
+            <p>{object.metadata.about.description}</p>
+          </article>
 
-        <div className={styles.bannerAbout}>
-          <Image
-            className={styles.imageAbout}
-            alt="Imagem ilustrativa sobre a empresa"
-            quality={100}
-            fill
-            src={object.metadata.about.banner.url}
-          />
-        </div>
-      </section>
-    </Container>
+          <div className={styles.bannerAbout}>
+            <Image
+              className={styles.imageAbout}
+              alt="Imagem ilustrativa sobre a empresa"
+              quality={100}
+              fill
+              src={object.metadata.about.banner.url}
+            />
+          </div>
+        </section>
+      </Container>
+
+      <div className={styles.backgroundServices}>
+        <Container>
+          <h2 className={styles.servicesTitle}>Conheça nossos serviços</h2>
+          
+          <section className={styles.services}>
+            {object.metadata.services.map((service, index) => (
+              <div className={styles.service}>
+                <div key={index} className={styles.innerService}>
+                  <Image
+                    className={styles.imageService}
+                    alt="Imagem do serviço"
+                    quality={100}
+                    fill
+                    src={service.image.url}
+                  />
+                </div>
+                <p>{service.description}</p>
+              </div>
+            ))}
+          </section>
+        </Container>
+      </div>
+    </>
   )
 }
 
